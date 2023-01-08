304c55558f1809d44c657c7018abd3403d15638d
Tomy <tomyhelda05@gmail.com>
2023-01-08 17:18:39 +0700

    unregister Group

--- core/settings.py
+++ core/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'static'),
     ]
 
 # Default primary key field type
--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 from .models import *
 # Register your models here.
+
+
+admin.site.unregister(Group)
 
 
 class CustomUserAdmin(UserAdmin):
